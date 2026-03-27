714aa2111fe9a47d5fb5e02856c4f3e542f900fa
Mariano <mcarpintero@icloud.com>
2026-03-27 15:39:19 +0100

    Documentación

--- results2json/trace.py
+++ results2json/trace.py
@@ -1,3 +1,15 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (c) 2026 Mariano Carpintero
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 from __future__ import annotations
 
 import json
